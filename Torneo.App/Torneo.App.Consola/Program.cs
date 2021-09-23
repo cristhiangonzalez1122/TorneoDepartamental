6221aa0317d04090cbd301e659fc1ffc8d290b00
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-09-23 12:18:51 -0500

    Creado el crud de Municipio

--- Torneo.App/Torneo.App.Consola/Program.cs
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -15,6 +15,9 @@ namespace Torneo.App.Consola
             Console.WriteLine("Hello Test");
             //AddMunicipio();
             //DeleteMunicipio(2);
+            //GetMunicipio(4);
+            //GetAllMunicipios();
+            UpdateMunicipio(4, "Jamundi");
         }
 
         private static void AddMunicipio()
@@ -22,8 +25,25 @@ namespace Torneo.App.Consola
             var municipio = new Municipio
             {
                 NombreMunicipio = "Cali",
+
             };
             _repoMunicipio.AddMunicipio(municipio);
+        }
+
+        private static void GetMunicipio(int idMunicipio)
+        {
+            var municipio = _repoMunicipio.GetMunicipio(idMunicipio);
+            System.Console.WriteLine("Nombre del municipio encontrado: " + municipio.NombreMunicipio);
+        }
+
+        private static void GetAllMunicipios()
+        {
+            var municipios = _repoMunicipio.GetAllMunicipios();
+            foreach (var municipio in municipios)
+            {
+                Console.WriteLine(municipio.NombreMunicipio);
+            }
+
         }
 
         private static void DeleteMunicipio(int idMunicipio)
@@ -31,6 +51,15 @@ namespace Torneo.App.Consola
             _repoMunicipio.DeleteMunicipio(idMunicipio);
             System.Console.WriteLine("Se borró el municipio con id: " + idMunicipio);
         }
+
+        private static void UpdateMunicipio(int idMunicipio, string nombreNuevo)
+        {
+            Municipio municipioEncontrado = _repoMunicipio.GetMunicipio(idMunicipio);
+            municipioEncontrado.NombreMunicipio = nombreNuevo;
+            Municipio municipioUpdated = _repoMunicipio.UpdateMunicipio(municipioEncontrado);
+        }
+
+
 
 
 
